b81be836371788da2a4dd87a3bb5aadbe9c0b5cd
Jonathon <jnw777@yahoo.com>
2021-01-19 21:40:04 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template, jsonify
+from markupsafe import escape
+from matplotlib.figure import Figure
+from io import BytesIO
+import base64
+import os
+import psycopg2
+import csv
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+@app.route("/meow")
+def meow():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+    
+    
+wport = int(os.getenv("PORT"))
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = True
+    app.run(host='0.0.0.0', port=wport)
